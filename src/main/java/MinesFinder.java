6dd0875fb7d5c70042c0262cfefee128fca6afcf
Edgar Bagagem <2002edgargomes@gmail.com>
2022-03-09 10:12:15 +0000

    Layout Básico

--- src/main/java/MinesFinder.java
+++ src/main/java/MinesFinder.java
@@ -4,6 +4,20 @@ import java.awt.*;
 public class MinesFinder extends JFrame {
 
     private JPanel painelPrincipal;
+    private JLabel lblNorte;
+    private JPanel jPanelWest;
+    private JLabel lblRecordes;
+    private JLabel lblFacil;
+    private JLabel lblJogadorFacil;
+    private JLabel lblMedio;
+    private JLabel lblJogadorMedio;
+    private JLabel lblDificil;
+    private JLabel lblJogadorDificil;
+    private JPanel jPanelCentro;
+    private JButton buttonSair;
+    private JButton buttonMedio;
+    private JButton buttonFacil;
+    private JButton buttonDificil;
 
     public MinesFinder(String title) {
         super(title);
